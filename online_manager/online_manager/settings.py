844ed56bce4b8b74bda754f687f5937da7e9c950
Carlos Hernan Arce <carlosap256@gmail.com>
2018-10-10 14:09:24 +0200

    Added login and logout pages and logic

--- online_manager/online_manager/settings.py
+++ online_manager/online_manager/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'task_list',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_list',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
--- online_manager/task_list/views.py
+++ online_manager/task_list/views.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
@@ -5,7 +7,15 @@ from django.contrib.auth.decorators import login_required
 from task_list.models import Task, User
 
 
+class TaskList:
+    pending_by_user: List[Dict[str, object]]
+    pending_by_others: List[Dict[str, object]]
+    done_by_user: List[Dict[str, object]]
+    done_by_other: List[Dict[str, object]]
+
+
 class Index(TemplateView):
+    task_list: TaskList
 
     def get(self, request, *args, **kwargs):
         tasks = self._get_tasks()
