11e6e1fde1fa129fdf5e253d570adc27deae3e25
Carlos Hernan Arce <carlosap256@gmail.com>
2018-10-12 16:25:22 +0200

    Complete tests

--- online_manager/task_list/task_crud.py
+++ online_manager/task_list/task_crud.py
@@ -1,10 +1,88 @@
 from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, HttpResponse, Http404
+from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, HttpResponse
 
 from task_list.mixin import LoginRequiredMixin
 from task_list.models import Task, User
+
+
+class CrudHandler:
+    user: User
+    method: str
+    task_id: int
+    task_name: str
+    valid_methods = ['create', 'edit', 'delete', 'done', 'undone']
+
+    def __init__(self, user: User, method: str):
+        self.user = user
+        self.method = method
+
+    def set_data(self, task_id: int = None, task_name: str = None):
+        self.task_id = task_id
+        self.task_name = task_name
+
+    def exec_method(self):
+        if self.method not in self.valid_methods:
+            return HttpResponseBadRequest()
+        else:
+            if self.method == 'create':
+                return self._exec_method_create()
+
+            if self.task_id is None:
+                return HttpResponseBadRequest()
+            try:
+                task_to_modify = Task.objects.get(id=self.task_id)
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound()
+
+            if self.method == 'done':
+                return self._mark_as_done(task_to_modify)
+            if self.method == 'undone':
+                return self._mark_as_undone(task_to_modify)
+            if self.method == 'delete':
+                return self._delete_task(task_to_modify)
+
+            if self.task_name is None:
+                return HttpResponseBadRequest()
+            if self.method == 'edit':
+                return self._edit_task(task_to_modify)
+
+    def _exec_method_create(self):
+        if self.task_name is None:
+            return HttpResponseBadRequest()
+        else:
+            self._create_task()
+            return HttpResponse()
+
+    def _create_task(self):
+        Task(name=self.task_name, owner=self.user, is_done=False).save()
+
+    def _edit_task(self, task: Task):
+        if task.can_modify(self.user):
+            task.name = self.task_name
+            task.save()
+            return HttpResponse()
+        else:
+            return HttpResponseForbidden()
+
+    def _delete_task(self, task: Task):
+        if task.can_modify(self.user):
+            task.delete()
+            return HttpResponse()
+        else:
+            return HttpResponseForbidden()
+
+    def _mark_as_done(self, task: Task):
+        task.is_done = True
+        task.marked_done_by = self.user
+        task.save()
+        return HttpResponse()
+
+    def _mark_as_undone(self, task: Task):
+        task.is_done = False
+        task.marked_done_by = None
+        task.save()
+        return HttpResponse()
 
 
 class TaskCrud(LoginRequiredMixin, TemplateView):
@@ -16,74 +94,23 @@ class TaskCrud(LoginRequiredMixin, TemplateView):
     def post(self, request, *args, **kwargs):
         user = self._get_user(request)
         method = self._get_request_method(kwargs)
+        task_id = self._get_task_id(request)
+        task_name = self._get_task_name(request)
 
-        if method == 'create':
-            task_name = self._get_task_name(request)
-            self._create_task(user, task_name)
-        else:
-            task = self._get_edited_task(request)
-            if method == 'edit':
-                task_name = self._get_task_name(request)
-                self._edit_task(task.id, task_name, user)
-            elif method == 'delete':
-                self._delete_task(task, user)
-            elif method == 'done':
-                self._mark_as_done(task, user)
-            elif method == 'undone':
-                self._mark_as_undone(task)
-            else:
-                return HttpResponseNotFound('<h1>Page not found</h1>')
+        crud_handler = CrudHandler(user, method)
+        crud_handler.set_data(task_id, task_name)
 
-        return render(request, 'index.html', {})
+        return crud_handler.exec_method()
 
     def _get_user(self, request):
         return request.user
 
     def _get_request_method(self, kwargs):
         method = kwargs.get('method', '')
-        if method not in self.valid_methods:
-            raise Http404
         return method
 
-    def _get_edited_task(self, request) -> Task:
-        task_id = self._get_task_id(request)
-        try:
-            return Task.objects.get(id=task_id)
-        except ObjectDoesNotExist:
-            raise Http404
-
     def _get_task_id(self, request) -> int:
-        if 'task_id' in request.POST:
-            return request.POST['task_id']
-        raise HttpResponseBadRequest
+        return request.POST.get('task_id', None)
 
     def _get_task_name(self, request) -> str:
-        if 'task_name' in request.POST:
-            return request.POST['task_name']
-        else:
-            raise HttpResponseBadRequest
-
-    def _create_task(self, user: User, name: str):
-        Task(name=name, owner=user, is_done=False).save()
-
-    def _edit_task(self, task_id: int, task_name: str, user: User):
-        task = Task.objects.get(id=task_id)
-        if task.can_modify(user):
-            task.name = task_name
-            task.save()
-
-    def _delete_task(self, task: Task, user: User):
-        if task.can_modify(user):
-            task.delete()
-        else:
-            raise HttpResponseForbidden
-
-    def _mark_as_done(self, task: Task, user: User):
-        task.is_done = True
-        task.marked_done_by = user
-        task.save()
-
-    def _mark_as_undone(self, task: Task):
-        task.is_done = False
-        task.marked_done_by = None
-        task.save()
+        return request.POST.get('task_name', None)
--- online_manager/task_list/tests.py
+++ online_manager/task_list/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, RequestFactory
+from django.http import HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, HttpResponse, Http404
 
 from task_list.views import FilteredTasks
 from task_list.task_crud import TaskCrud
@@ -36,7 +37,7 @@ class TaskFilterTest(TestCase):
         pass
 
 
-class TaskCrudTest(TestCase):
+class TaskCrudNormalTest(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
@@ -55,6 +56,7 @@ class TaskCrudTest(TestCase):
 
         created_task = Task.objects.filter(owner=self.user, name=data['task_name'])
         assert len(created_task) is 1
+        assert response.status_code == HttpResponse.status_code
 
     def test_edit(self):
         data = {'task_id': self.task.id, 'task_name': 'new_task_name'}
@@ -65,6 +67,7 @@ class TaskCrudTest(TestCase):
 
         modified_task = Task.objects.get(id=self.task.id)
         assert modified_task.name == data['task_name']
+        assert response.status_code == HttpResponse.status_code
 
     def test_delete_method(self):
         task_id = self.task.id
@@ -76,6 +79,7 @@ class TaskCrudTest(TestCase):
 
         deleted_task = Task.objects.filter(id=task_id)
         assert len(deleted_task) is 0
+        assert response.status_code == HttpResponse.status_code
 
     def test_mark_done(self):
         data = {'task_id': self.task.id}
@@ -87,6 +91,7 @@ class TaskCrudTest(TestCase):
         modified_task = Task.objects.get(id=self.task.id)
         assert modified_task.is_done is True
         assert modified_task.marked_done_by.username == self.user.username
+        assert response.status_code == HttpResponse.status_code
 
     def test_mark_undone(self):
         data = {'task_id': self.done_task.id}
@@ -98,6 +103,101 @@ class TaskCrudTest(TestCase):
         modified_task = Task.objects.get(id=self.done_task.id)
         assert modified_task.is_done is False
         assert modified_task.marked_done_by is None
+        assert response.status_code == HttpResponse.status_code
 
     def tearDown(self):
         pass
+
+
+class TaskCrudErrorsTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        self.user2 = User.objects.create_user(username='test_user2', password='test_password')
+        self.task = Task(name='Test task', owner=self.user, is_done=False)
+        self.task.save()
+        self.done_task = Task(name='Test done task', owner=self.user, is_done=True, marked_done_by=self.user)
+        self.done_task.save()
+
+    def test_invalid_method(self):
+        data = {}
+        kwargs = {'method': 'invalid method'}
+        request = self.factory.post(path='/task_crud/create/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_empty_create_request(self):
+        data = {}
+        kwargs = {'method': 'create'}
+        request = self.factory.post(path='/task_crud/create/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_empty_done_request(self):
+        data = {}
+        kwargs = {'method': 'done'}
+        request = self.factory.post(path='/task_crud/done/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_empty_undone_request(self):
+        data = {}
+        kwargs = {'method': 'undone'}
+        request = self.factory.post(path='/task_crud/undone/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_empty_edit_request(self):
+        data = {}
+        kwargs = {'method': 'edit'}
+        request = self.factory.post(path='/task_crud/edit/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_empty_delete_request(self):
+        data = {}
+        kwargs = {'method': 'delete'}
+        request = self.factory.post(path='/task_crud/delete/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseBadRequest.status_code
+
+    def test_unexisting_id_request(self):
+        data = {'task_id': 999, 'task_name': self.task.name}
+        kwargs = {'method': 'edit'}
+        request = self.factory.post(path='/task_crud/edit/', data=data, )
+        request.user = self.user
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseNotFound.status_code
+
+    def test_forbidden_edit(self):
+        data = {'task_id': self.task.id, 'task_name': self.task.name}
+        kwargs = {'method': 'edit'}
+        request = self.factory.post(path='/task_crud/edit/', data=data, )
+        request.user = self.user2
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseForbidden.status_code
+
+    def test_forbidden_delete(self):
+        data = {'task_id': self.task.id, 'task_name': self.task.name}
+        kwargs = {'method': 'delete'}
+        request = self.factory.post(path='/task_crud/delete/', data=data, )
+        request.user = self.user2
+        response = TaskCrud.as_view()(request, **kwargs)
+
+        assert response.status_code == HttpResponseForbidden.status_code
+
